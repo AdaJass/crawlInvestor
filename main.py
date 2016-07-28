e4bd966aa76f7f533bce296b5862cc22954642ec
unknown <>
2016-07-28 17:55:59 +0800

    akjg

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ import config
 import processData as pd
 import sys
 
-async def fetchData(pages, callback = pd.processData):
+
+from openpyxl import Workbook #写入excel使用(支持07)
+from openpyxl.writer.excel import ExcelWriter
+
+async def fetchData(callback = pd.processData):
     #set request url and parameters here or you can pass from outside. 
     
     #use s.** request a webside will keep-alive the connection automaticaly,
@@ -14,23 +18,80 @@ async def fetchData(pages, callback = pd.processData):
     #await s.get('***/page1')
     #await s.get('***/page2')
     ######################################################################## 
-    cookies = {}
+    cookies = {
+        'gr_user_id' : '87b14305-11d0-48b6-bd8f-4d243cabe01d',
+        'pgv_pvi' : '9987699712',
+        'identity' : 'jass.ada%40qq.com',
+        'remember_code' : '5QE6q7WHRi',
+        'acw_tc' : 'AQAAAJLiJ0xmFwMAZDvwcrBeFZnFtCMH',
+        'session': '93809e53d60b2986d23789a699e11244ea55e280',
+        'pgv_si' : 's1540547584',
+        '_gat' :'1',
+        'gr_session_id_eee5a46c52000d401f969f4535bdaa78': '0d721d63-71f2-4a3d-ac45-4d9d12ddb9fc',
+        '_ga' : 'GA1.2.125227903.1457342767',
+        'Hm_lvt_1c587ad486cdb6b962e94fc2002edf89' : '1469520678,1469522213,1469675226',
+        'Hm_lpvt_1c587ad486cdb6b962e94fc2002edf89' : '1469675235'
+    }
 
     conn = aiohttp.TCPConnector(limit=config.REQ_AMOUNTS)    
     s = aiohttp.ClientSession(headers = config.HEADERS, cookies=cookies, connector=conn)   
 
     url = 'https://www.itjuzi.com/investfirm/'
-    while  pages<6484:        
-        async with s.get(url+str(pages+1)) as r:
+    AllData=[]
+    pages=1
+    r='text'
+    while  pages<=6484:
+        coroutines = [s.get(url+str(pages+i)) for i in range(10)]                
+        for coroutine in asyncio.as_completed(coroutines):             
+            try:
+                r = await coroutine
+            except Exception:
+                await asyncio.sleep(2)
+                r = await coroutine
+            if not r:
+                continue
             data =  await r.text(encoding='utf-8')
-            investor = await callback(data, s)
-        pages=pages+17
+            AllData.append(await callback(data, s))
 
-if __name__ == '__main__':
-    
-    loop = asyncio.get_event_loop()
-    #total invest departments has 6484
-    #coroutine in tasks will run 
-    tasks = [fetchData(i, pd.processData) for i in range(17)]    
+        pages=pages+10
+        # await asyncio.sleep(1)
+        
+
+################################################################
+################################################################
+
+    filename = 'InstitutionVCDB.xlsx'
+    sheetname = 'Institution'
+    wb = Workbook()                            #新建一个文件
+    wb_writer = ExcelWriter(workbook = wb)     #用来写入文件
+    ws = wb.worksheets[0]                      #新建一个sheet
+    ws.title = sheetname                       #定义sheet名称
+        
+    for i, v in enumerate(AllData):      #表示遍历行数(最后一个数字循环不到)
+        ws.cell(row=i+1,column=1).value= v['title'] 
+        ws.cell(row=i+1,column=2).value= v['descript']
+        tem=v['investField'] 
+        for num in range(6):
+            if num >= len(tem):
+                break
+            ws.cell(row=i+1,column=3+num).value = tem[num]
+
+        tem=v['investTurn'] 
+        for num in range(4):
+            if num >= len(tem):
+                break
+            ws.cell(row=i+1,column=9+num).value = tem[num]
+
+        ws.cell(row=i+1,column=13).value= v['url']
+        ws.cell(row=i+1,column=14).value= v['phone']
+        ws.cell(row=i+1,column=15).value= v['email']
+        ws.cell(row=i+1,column=16).value= v['address']
+        
+    wb_writer.save(filename = filename)        #保存写入文件
+
+
+if __name__ == '__main__':    
+    loop = asyncio.get_event_loop()    
+    tasks = [fetchData(pd.processData)]    
     loop.run_until_complete(asyncio.wait(tasks))
     loop.close() 
--- processData.py
+++ processData.py
@@ -2,11 +2,12 @@ import asyncio
 from pyquery import PyQuery as pq
 import config
 
+
 async def processData(data,session):
     '''
     data is from the http response in main module.
     '''
-    d = pd(data)
+    d = pq(data)
     title=d('div.picinfo span.title').text()
     url=d('div.picinfo span.links a').eq(1).text()
     wechat = d('ul.aboutus span').eq(0).text()
@@ -18,16 +19,34 @@ async def processData(data,session):
 
     basicInfo = d('div.block-inc-info').text()
     investField=[]
-    for tag in d('div.pad div.darkblue b'):
+    for tag in d('div.pad div.darkblue b').items():
         investField.append(tag.text())
 
     investTurn=[]
-    for tag in d('div.pad div.yellow b'):
+    for tag in d('div.pad div.yellow b').items():
         investTurn.append(tag.text())
 
     investors=[]
-    for tag in d('ul.list-prodcase li b'):
+    for tag in d('ul.list-prodcase li b').items():
         investors.append(tag.text())
-        
-            
 
+    print(title)
+    # print(basicInfo)
+    # print(investTurn)
+    # print(investors)
+    result={
+        'title': title,
+        'url': url,
+        'phone': phone,
+        'email': email,
+        'address': address,
+        'descript': basicInfo,
+        'investField': investField,
+        'investTurn': investTurn,
+        'investor': investors
+    }
+
+    return result
+
+      
+    
